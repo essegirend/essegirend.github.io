6274bda33f926878f9b7596da3e67ef1a25a5b71
Nicola Di Gregorio <nicoladigregorio@MacBook-Pro-di-Nicola.local>
2024-06-13 19:23:24 +0200

    fine

--- static/js/list.js
+++ static/js/list.js
@@ -50,7 +50,7 @@ dispreq.onload = function () {
 }
 
 function golist() {
-  if(new Date(fromlist.value) > new Date(tolist.value) || fromlist.value == "" || tolist.value == "" || new Date(fromlist.value) < new Date() || new Date(tolist.value) < new Date()){
+  if(new Date(fromlist.value) > new Date(tolist.value) || fromlist.value == "" || tolist.value == "" || new Date(fromlist.value).getTime() < new Date().setHours("00", "00", "00", "00") || new Date(tolist.value).getTime() < new Date().setHours("00", "00", "00", "00")){
     fromlist.classList.add("outline-2" , "shadow-md", "shadow-red-500", "border-red-500") 
     tolist.classList.add("outline-2" , "shadow-md", "shadow-red-500", "border-red-500")
     return;
