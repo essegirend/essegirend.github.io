81f203eee5dc689e99c1fbf694d371c38e11a58f
Nicola Di Gregorio <nicoladigregorio@192.168.1.54>
2024-06-19 22:27:32 +0200

    grest

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+const { red } = require("color-name");
 
 
 const oggi = new XMLHttpRequest();
@@ -68,8 +69,8 @@ cross.addEventListener("click", toggleMenu);
 function golist() {
 
   if(new Date(from.value) > new Date(to.value) || from.value == "" || to.value == "" || new Date(from.value).getTime() < new Date().setHours("00", "00", "00", "00") || new Date(to.value).getTime() < new Date().setHours("00", "00", "00", "00")){
-    from.classList.add("border-2", "shadow-lg", "shadow-red-500", "border-red-500") 
-    to.classList.add("border-2", "shadow-lg", "shadow-red-500", "border-red-500")
+    from.style.borderColor = "red";
+    to.style.borderColor = "red";
     return;
   }
   let alve = new URL("https://shy-neon.github.io/list.html");
